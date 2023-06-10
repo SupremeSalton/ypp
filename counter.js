049f967964e24004b6ede321bb46324218ababa6
SupremeSalton <arturalesh@gmail.com>
2023-06-10 13:43:51 +0700

    lala

--- counter.js
+++ counter.js
@@ -1,7 +1,7 @@
 import { flatten } from "lodash"
 
 export function setupCounter(element) {
-  console.log(flatten([[[1,2,3,4,5,6,7,8]]]));
+  console.log(flatten([[[1,2,3,4,5,6,7,8,9,10]]]));
   let counter = 0
   const setCounter = (count) => {
     counter = count
