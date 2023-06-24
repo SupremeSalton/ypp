83e755cde35b6df490581e508a10a1ea3476e2a3
SupremeSalton <arturalesh@gmail.com>
2023-06-24 12:45:59 +0700

    end of mess

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
-=======
+
 import {resolve} from 'path'
 import { defineConfig } from "vite";
 import handlebars from "vite-plugin-handlebars";
 
->>>>>>> 4c48d9e3 (mess)
 export default defineConfig({
     root: resolve(__dirname, 'src'),
     build: {
@@ -17,9 +15,5 @@ export default defineConfig({
         host: 'localhost',
         port: '3000'
     }
+})
 
-<<<<<<< HEAD
-})
-=======
-})
->>>>>>> 4c48d9e3 (mess)
