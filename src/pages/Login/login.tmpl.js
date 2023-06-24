2e8441386136585a39395949cad24d8d28a5808f
SupremeSalton <arturalesh@gmail.com>
2023-06-24 11:45:01 +0700

    mess1

--- src/pages/Login/login.tmpl.js
+++ src/pages/Login/login.tmpl.js
@@ -11,7 +11,6 @@ export const template =  `
     </main>
 
     <ul class="link_list">
-<<<<<<< HEAD
         <li> <a href="https://dreamy-chaja-f6b77c.netlify.app/login"> Login (/login)</a></li>
         <li> <a href="https://dreamy-chaja-f6b77c.netlify.app/registration"> Registration (/registration) </a></li>
         <li> <a href="https://dreamy-chaja-f6b77c.netlify.app/chats"> Chats (/chats) </a></li>
@@ -20,15 +19,5 @@ export const template =  `
         <li> <a href="https://dreamy-chaja-f6b77c.netlify.app/passwordchange"> Change Password (/passwordchange) </a></li>
         <li> <a href="https://dreamy-chaja-f6b77c.netlify.app/404"> 404 (/404)</a></li>
         <li> <a href="https://dreamy-chaja-f6b77c.netlify.app/500"> 500 (/500)</a></li>
-=======
-        <li> <a href="http://localhost:3000/login"> Login </a></li>
-        <li> <a href="http://localhost:3000/registration"> Registration </a></li>
-        <li> <a href="http://localhost:3000/chats"> Chats </a></li>
-        <li> <a href="http://localhost:3000/profile"> Profile </a></li>
-        <li> <a href="http://localhost:3000/settings"> Settings </a></li>
-        <li> <a href="http://localhost:3000/passwordchange"> Change Password </a></li>
-        <li> <a href="http://localhost:3000/404"> 404 </a></li>
-        <li> <a href="http://localhost:3000/500"> 500 </a></li>
->>>>>>> 4c48d9e3 (mess)
     </ul>
 `
